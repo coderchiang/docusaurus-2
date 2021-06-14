f33970fa17c534360e426de0ce7780ffd362f284
chris <2501170033@qq.com>
2021-06-14 18:31:30 +0800

    first

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,19 +8,19 @@ module.exports = {
   projectName: 'docusaurus', // Usually your repo name.
   themeConfig: {
     navbar: {
-      title: 'My Site',
+      title: 'GinAdmin',
       logo: {
-        alt: 'My Site Logo',
+        alt: 'gin-admin',
         src: 'img/logo.svg',
       },
       links: [
         {
-          to: 'docs/doc1',
+          to: 'docs/README.zh-CN',
           activeBasePath: 'docs',
-          label: 'Docs',
+          label: '在线文档',
           position: 'left',
         },
-        {to: 'blog', label: 'Blog', position: 'left'},
+        {to: 'blog/aboutus', label: '关于我们', position: 'left'},
         {
           href: 'https://github.com/facebook/docusaurus',
           label: 'GitHub',
@@ -32,45 +32,46 @@ module.exports = {
       style: 'dark',
       links: [
         {
-          title: 'Docs',
+          title: '项目文档',
           items: [
             {
-              label: 'Style Guide',
-              to: 'docs/doc1',
+              label: '中文文档',
+              to: 'docs/README.zh-CN',
             },
             {
-              label: 'Second Doc',
-              to: 'docs/doc2',
+              label: 'ENGLISH DOCUMENTATION',
+              to: 'docs/README',
             },
           ],
         },
         {
-          title: 'Community',
+          title: '社区',
           items: [
             {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+              label: 'bbs社区',
+              href: 'https://bbs.iznjy.cn',
             },
             {
-              label: 'Discord',
+              label: 'QQ群',
               href: 'https://discordapp.com/invite/docusaurus',
             },
-            {
-              label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
-            },
+          
           ],
         },
         {
-          title: 'More',
+          title: '更多',
           items: [
             {
-              label: 'Blog',
-              to: 'blog',
+              label: '关于我们',
+              to: 'blog/aboutus',
             },
             {
               label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
+              href: 'https://github.com/coderchiang/gin-admin',
+            },
+            {
+              label: 'Gitee',
+              href: 'https://gitee.com/Chris_Chiang/gin-admin',
             },
           ],
         },
@@ -89,10 +90,56 @@ module.exports = {
             'https://github.com/facebook/docusaurus/edit/master/website/',
         },
         blog: {
+          path: 'blog',
+          /**
+           * 编辑您站点的 URL。
+           * Docusaurus 将使用 "editUrl + relativeDocPath" 来自动计算最终的 editUrl
+           */
+          editUrl: 'https://github.com/facebook/docusaurus/edit/master/website/',
+          /**
+           * 您也可以自己根据每个 Markdown 文件进行编辑 Url。
+           */
+        
+          /**
+           * 在提交本地文件至 Git 时有用。
+           * 本地化 Markdown 文件后，编辑 URl 将指定为本地化后的文件，
+           * 而非未本地化的原始文件。
+           * 注：此选项将在 editUrl 为函数时被自动忽略
+           */
+          editLocalizedFiles: false,
+          /**
+           * 用以优化 SEO 的博客页面标题
+           */
+          blogTitle: 'Blog title',
+          /**
+           * 用以优化 SEO 的博客页面源描述
+           */
+          blogDescription: 'Blog',
+          /**
+           * 显示于博客侧边栏的博文数量，设置为
+           * 'ALL' 以显示所有博文，0 则禁用此功能
+           */
+          blogSidebarCount: 'ALL',
+          /**
+           * 博客侧边栏标题
+           */
+          blogSidebarTitle: 'All our posts',
+          /**
+           * 通往您博客的 URL 路由。
+           * *请务必不要*添加斜杠。
+           */
+          routeBasePath: 'blog',
+          include: ['*.md', '*.mdx'],
+          postsPerPage: 10,
+          /**
+           * 博客页面所使用的主题组件。
+           */
+          blogListComponent: '@theme/BlogListPage',
+          blogPostComponent: '@theme/BlogPostPage',
+          blogTagsListComponent: '@theme/BlogTagsListPage',
+          blogTagsPostsComponent: '@theme/BlogTagsPostsPage',
           showReadingTime: true,
           // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,5 @@
 module.exports = {
   someSidebar: {
-    Docusaurus: ['doc1', 'doc2', 'doc3'],
-    Features: ['mdx'],
+    项目文档: ['README.zh-CN','README'],
   },
 };
